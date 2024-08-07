fa630d937b86a6e9e8062cd16fcc50d419ec2c02
rana saad <bitf21m041@pucit.edu.pk>
2024-08-07 22:36:34 +0500

    making 3 routes of owner products and users

---
+++ models/productModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    name: String,
+    panelcolor: String,
+    textcolor: String,
+    bgcolor: String,
+    discount: {
+        type: Number,
+        default: 0
+    },
+    price: Number,
+    image: String
+})
+
+module.exports = mongoose.model("product", productSchema);
