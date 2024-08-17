020b65582ffe887139eb41d6194d2ca531100316
rana saad <bitf21m041@pucit.edu.pk>
2024-08-17 14:06:37 +0500

    ok

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const app = express();
 
 const cookieParser = require("cookie-parser");
--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const config = require("config");
 mongoose
-    .connect("mongodb://127.0.0.1:27017/scatch")
+    .connect(`${config.get("MONGODB_URI")}/scatch`)
     .then(function () {
         console.log("connected to mongodb server");
     })
